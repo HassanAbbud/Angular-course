321a08fb5a2a9a3d3f29b310e3cfa39882efe4ec
Hassan Sharif Abbud González <hassanabbud@hotmail.com>
2024-07-04 00:53:53 -0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes/heroes.module';
-
+import { DbzModule } from './dbz/services/dbz.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Character } from '../interfaces/charcter.interface';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent {
+  public characters: Character[] = [{
+    name: "Krillin",
+    power: 5000
+  },{
+    name: "Goku",
+    power: 9500
+  },{
+    name: "Trunks",
+    power: 50
+  }];
+
+}
